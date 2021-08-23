4b065eb6e15dfe4dd45054e185766edaaa0e8d39
Yan David <ygomezcoba@gmail.com>
2021-08-23 16:32:41 -0400

    Change the order to take the PORT

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000 || process.env.PORT;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Running on port: ${port}`)
 }
