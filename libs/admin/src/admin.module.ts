cbb7c300880f85f5c3b38c1ef7241402d38e3af8
javierrod9191 <javierrod9191@gmail.com>
2021-08-24 13:45:59 -0500

    FB-7 chore: import SkillModule to the admin Module

--- libs/admin/src/admin.module.ts
+++ libs/admin/src/admin.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AdminService } from './admin.service'
+import { SkillModule } from './skill/skill.module';
 
 @Module({
   providers: [AdminService],
   exports: [AdminService],
+  imports: [SkillModule],
 })
 export class AdminModule {}
