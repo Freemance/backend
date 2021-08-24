973ebb8bc549448f135143d6defdbc6b902859f0
javierrod9191 <javierrod9191@gmail.com>
2021-08-24 15:56:24 -0500

    FB-7 fix: skill model class

--- libs/admin/src/skill/model/skill.ts
+++ libs/admin/src/skill/model/skill.ts
@@ -2,21 +2,21 @@ import { Field, ObjectType, Int } from '@nestjs/graphql'
 
 @ObjectType()
 export class Skill {
-  @Field((type) => Int, { description: 'Skill id' })
-  id: number
+  @Field((type) => Int, { description: 'Skill id', nullable: true })
+  id?: number
 
-  @Field()
-  createdAt: Date
+  @Field({ nullable: true })
+  createdAt?: Date
 
-  @Field()
-  updatedAt: Date
+  @Field({ nullable: true })
+  updatedAt?: Date
 
-  @Field({ description: 'Skill name' })
-  name: string
+  @Field({ description: 'Skill name', nullable: true })
+  name?: string
 
   @Field((type) => [Profile], { description: 'Profiles asociated to Skill', nullable: 'items' })
-  profiles: Profile[]
+  profiles?: Profile[]
 
-  @Field((type) => Int, { description: 'Portfolio id asociated to Skill', nullable: true })
-  portfolio?: number
+  @Field({ description: 'Portfolio id asociated to Skill', nullable: true })
+  portfolio?: Portfolio
 }
