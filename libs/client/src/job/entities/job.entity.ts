60b0fadeaa225a624d75372f1d2c234f102c9940
Yan David <ygomezcoba@gmail.com>
2021-08-25 19:52:51 -0400

    FB-13 chore: add realtion job-profile to entity

--- libs/client/src/job/entities/job.entity.ts
+++ libs/client/src/job/entities/job.entity.ts
@@ -1,7 +1,12 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql'
+import { BaseModel } from '@feature/core'
+import { Profile } from '@feature/client'
 
 @ObjectType()
-export class Job {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class Job extends BaseModel {
+  @Field({ description: 'Job name' })
+  name: string
+
+  @Field(() => [Profile], { description: 'Profiles asociated to Jobs', nullable: 'items' })
+  profiles: Profile[]
 }
