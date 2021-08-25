4e2ad9da53fe1ac44c1cbabbd3acc7c988b91ebc
javierrod9191 <javierrod9191@gmail.com>
2021-08-25 14:00:41 -0500

    FB-14 refactor: tag entitiy

--- libs/admin/src/tag/entities/tag.entity.ts
+++ libs/admin/src/tag/entities/tag.entity.ts
@@ -1,7 +1,12 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql'
+import { BaseModel } from '@feature/core'
+import { Portfolio } from '@feature/client'
 
 @ObjectType()
-export class Tag {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number
+export class Tag extends BaseModel {
+  @Field({ description: 'Tag name' })
+  name: string
+
+  @Field(() => [Profile], { description: 'Profiles asociated to Skill', nullable: 'items' })
+  profiles: Profile[]
 }
