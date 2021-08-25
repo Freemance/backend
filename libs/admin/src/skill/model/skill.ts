b5e1637a45603d789f4855902637af49d4109bdf
javierrod9191 <javierrod9191@gmail.com>
2021-08-25 10:41:31 -0500

    FB-7 refactor: skill model + dtos

--- libs/admin/src/skill/model/skill.ts
+++ libs/admin/src/skill/model/skill.ts
@@ -1,21 +1,13 @@
-import { Field, ObjectType, Int, GraphQLISODateTime } from '@nestjs/graphql'
+import { BaseModel } from '@feature/core'
+import { Field, ObjectType } from '@nestjs/graphql'
 
 @ObjectType()
-export class Skill {
-  @Field(() => Int, { description: 'Skill id', nullable: true })
-  id?: number
-
-  @Field(() => GraphQLISODateTime, { nullable: true })
-  createdAt?: Date
-
-  @Field(() => GraphQLISODateTime, { nullable: true })
-  updatedAt?: Date
-
-  @Field({ description: 'Skill name', nullable: true })
-  name?: string
+export class Skill extends BaseModel {
+  @Field({ description: 'Skill name' })
+  name: string
 
   @Field(() => [Profile], { description: 'Profiles asociated to Skill', nullable: 'items' })
-  profiles?: Profile[]
+  profiles: Profile[]
 
   @Field({ description: 'Portfolio id asociated to Skill', nullable: true })
   portfolio?: Portfolio
