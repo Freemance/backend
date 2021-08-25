1faaa07f199b18e95235cfd765480e1e753606b1
javierrod9191 <javierrod9191@gmail.com>
2021-08-25 10:55:36 -0500

    FB-14 fix: yarn format

--- libs/client/src/client.module.ts
+++ libs/client/src/client.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ClientService } from './client.service'
+import { PortfolioModule } from './portfolio/portfolio.module'
 
 @Module({
   providers: [ClientService],
   exports: [ClientService],
+  imports: [PortfolioModule],
 })
 export class ClientModule {}
