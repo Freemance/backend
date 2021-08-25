e93e8bd852049a5ba56301c17adee55e34684f0d
Yan David <ygomezcoba@gmail.com>
2021-08-25 12:00:50 -0400

    FB-14 refactor: tag model DTO validation

--- libs/admin/src/tag/dto/update-tag.input.ts
+++ libs/admin/src/tag/dto/update-tag.input.ts
@@ -1,8 +1,11 @@
 import { CreateTagInput } from './create-tag.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { IsString, MaxLength } from 'class-validator';
 
 @InputType()
 export class UpdateTagInput extends PartialType(CreateTagInput) {
-  @Field(() => Int)
-  id: number;
+  @Field(() => String, { nullable: true })
+  @IsString()
+  @MaxLength(100)
+  name: String;
 }
