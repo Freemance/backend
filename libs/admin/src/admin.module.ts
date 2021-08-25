cdd5d19c26d4ee0b744920d9aecd53a53e7968d7
javierrod9191 <javierrod9191@gmail.com>
2021-08-25 08:20:59 -0500

    FB-7 fix: add exports

--- libs/admin/src/admin.module.ts
+++ libs/admin/src/admin.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AdminService } from './admin.service'
-import { SkillModule } from './skill/skill.module'
-import { TagModule } from './tag/tag.module';
+import { SkillModule } from './skill'
+import { TagModule } from './tag/tag.module'
 
 @Module({
-  providers: [AdminService],
-  exports: [AdminService],
   imports: [SkillModule, TagModule],
+  providers: [],
+  exports: [],
 })
 export class AdminModule {}
