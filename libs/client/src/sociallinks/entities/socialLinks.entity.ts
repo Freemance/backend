f18683a4fdfb78c4645bf215977796db03e59184
javierrod9191 <javierrod9191@gmail.com>
2021-08-25 14:42:22 -0500

    FB-12 chore: add SocialLink model

--- libs/client/src/sociallinks/entities/socialLinks.entity.ts
+++ libs/client/src/sociallinks/entities/socialLinks.entity.ts
@@ -1,7 +1,17 @@
+import { BaseModel } from '@feature/core'
 import { ObjectType, Field, Int } from '@nestjs/graphql'
 
 @ObjectType()
-export class SocialLink {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number
+export class SocialLink extends BaseModel {
+  @Field({ description: 'Linkendin link', nullable: true })
+  linkedin?: string
+
+  @Field({ description: 'Instagram link', nullable: true })
+  instagram?: string
+
+  @Field({ description: 'Facebook link', nullable: true })
+  facebook?: string
+
+  @Field({ description: 'Telegram link', nullable: true })
+  telegram?: string
 }
