7033c3317a1df2c0ca70a76ac9fde3eff664dac6
Yan David <ygomezcoba@gmail.com>
2021-08-25 12:58:43 -0400

    FB-14  refactor: import DataModule to tag module

--- libs/admin/src/tag/tag.module.ts
+++ libs/admin/src/tag/tag.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TagService } from './service/tag.service';
 import { TagResolver } from './resolver/tag.resolver';
+import { DataModule } from '@feature/core';
 
 @Module({
+  imports: [DataModule],
   providers: [TagResolver, TagService]
 })
 export class TagModule {}
