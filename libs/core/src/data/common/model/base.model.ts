4733bbd0eeb2ec754714ef7337a2521d5da786df
javierrod9191 <javierrod9191@gmail.com>
2021-08-25 10:40:53 -0500

    FB-7 fix: chang return to Int

--- libs/core/src/data/common/model/base.model.ts
+++ libs/core/src/data/common/model/base.model.ts
@@ -1,13 +1,15 @@
-import { Field, ObjectType, ID, GraphQLISODateTime } from '@nestjs/graphql'
+import { Field, ObjectType, Int, GraphQLISODateTime } from '@nestjs/graphql'
 
 @ObjectType({ isAbstract: true })
 export abstract class BaseModel {
-  @Field(() => ID)
+  @Field(() => Int)
   id: number
+  
   @Field(() => GraphQLISODateTime, {
     description: 'Identifies the date and time when the object was created.',
   })
   createdAt: Date
+
   @Field(() => GraphQLISODateTime, {
     description: 'Identifies the date and time when the object was last updated.',
   })
