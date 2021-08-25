b732f4fcd8e6017a892804d9ca4e149c8ed9ff27
javierrod9191 <javierrod9191@gmail.com>
2021-08-25 11:09:48 -0500

    FB-14 chore: add portfolio model+updateAt,createAt

--- libs/client/src/portfolio/entities/portfolio.entity.ts
+++ libs/client/src/portfolio/entities/portfolio.entity.ts
@@ -1,7 +1,37 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql'
+import { ObjectType, Field, Int, GraphQLISODateTime } from '@nestjs/graphql'
+import { BaseModel } from '@feature/core'
+import { IsArray, IsDate, IsString } from 'class-validator'
+import { Skill } from '@feature/admin'
 
 @ObjectType()
-export class Portfolio {
+export class Portfolio extends BaseModel {
   @Field(() => Int, { description: 'Example field (placeholder)' })
   exampleField: number
+
+  @Field({ description: 'Name of the proyect', nullable: true })
+  @IsString()
+  proyect?: string
+
+  @Field({ description: 'Link associated  to the proyect', nullable: true })
+  @IsString()
+  link?: string
+
+  @Field({ description: 'Description associated  to the proyect', nullable: true })
+  @IsString()
+  description?: string
+
+  @Field(() => [Skill], { description: 'Description associated  to the proyect', nullable: true })
+  @IsArray()
+  skills?: [Skill]
+
+  @Field(() => GraphQLISODateTime, { description: 'Start date associated  to the proyect', nullable: true })
+  @IsDate()
+  startDate?: Date
+
+  @Field(() => GraphQLISODateTime, { description: 'End date associated  to the proyect', nullable: true })
+  @IsDate()
+  endDate?: Date
+
+  @Field(() => Profile, { description: 'Profile associated  to the proyect', nullable: true })
+  profile?: Profile
 }
