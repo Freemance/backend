af37da498367b8bedbb7fc7ba15142c29df2dfc7
Yoandry leon <yoandry@getauru.com>
2021-08-25 14:02:25 -0400

    FB-2 shore:implement Auth library with login/singup/refresh

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -1,8 +1,33 @@
+import { AuthResolver } from './auth.resolver'
 import { Module } from '@nestjs/common'
-import { AuthService } from './auth.service'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { JwtStrategy } from './jwt.strategy'
+import { ConfigService } from '@nestjs/config'
+import { DataModule } from '@feature/core'
+import { SecurityConfig } from '@feature/core/core/config/config.interface'
+import { AuthService } from '@feature/auth/auth.service'
+import { PasswordService } from '@feature/auth/password.service'
+import { GqlAuthGuard } from '@feature/auth/guards/gql-auth.guard'
 
 @Module({
-  providers: [AuthService],
-  exports: [AuthService],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => {
+        const securityConfig = configService.get<SecurityConfig>('security')
+        return {
+          secret: configService.get<string>('JWT_ACCESS_SECRET'),
+          signOptions: {
+            expiresIn: securityConfig.expiresIn,
+          },
+        }
+      },
+      inject: [ConfigService],
+    }),
+    DataModule,
+  ],
+  providers: [AuthService, AuthResolver, JwtStrategy, GqlAuthGuard, PasswordService],
+  exports: [GqlAuthGuard],
 })
 export class AuthModule {}
