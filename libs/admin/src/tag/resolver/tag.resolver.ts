7f870b740ba585fbeaff306530a08cbaa810141e
javierrod9191 <javierrod9191@gmail.com>
2021-08-25 13:58:23 -0500

    FB-14 refactor: tag resolver

--- libs/admin/src/tag/resolver/tag.resolver.ts
+++ libs/admin/src/tag/resolver/tag.resolver.ts
@@ -1,36 +1,32 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql'
-import { TagService } from '../service/tag.service'
-import { Tag } from '../entities/tag.entity'
-import { CreateTagInput } from '../dto/create-tag.input'
-import { UpdateTagInput } from '../dto/update-tag.input'
-import { ParseIntPipe } from '@nestjs/common'
+import { CreateTagInput, Tag, TagService, UpdateTagInput } from '..'
 
 @Resolver(() => Tag)
 export class TagResolver {
-  constructor(private readonly tagService: TagService) {}
+  constructor(private readonly service: TagService) {}
 
   @Mutation(() => Tag)
   createTag(@Args('createTagInput') createTagInput: CreateTagInput) {
-    return this.tagService.create(createTagInput)
+    return this.service.createTag(createTagInput)
   }
 
   @Query(() => [Tag], { name: 'tag' })
   getAllTag() {
-    return this.tagService.getAllTag()
+    return this.service.getAllTag()
   }
 
   @Query(() => Tag, { name: 'tag' })
-  getTagById(@Args('id', ParseIntPipe) id: number): String {
-    return this.tagService.getTagById(id)
+  getTagById(@Args('id', { type: () => Int }) id: number) {
+    return this.service.getTagById(id)
   }
 
   @Mutation(() => Tag)
-  updateTag(@Args('id', ParseIntPipe) id: number, @Args('input') input: UpdateTagInput) {
-    return this.tagService.update(id, input)
+  updateTag(@Args('id', { type: () => Int }) id: number, @Args('input') input: UpdateTagInput) {
+    return this.service.updateTag(id, input)
   }
 
-  @Mutation(() => Tag)
-  deleteTag(@Args('id', ParseIntPipe) id: number) {
-    return this.tagService.delete(id)
+  @Mutation(() => Boolean)
+  deleteTag(@Args('id', { type: () => Int }) id: number) {
+    return this.service.delete(id)
   }
 }
