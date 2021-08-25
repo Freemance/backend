3517ca9f6aa8db8dce392a0ccc33af77453edf6f
Yan David <ygomezcoba@gmail.com>
2021-08-25 11:58:29 -0400

    FB-14 refactor: add Max length validation

--- libs/admin/src/tag/dto/create-tag.input.ts
+++ libs/admin/src/tag/dto/create-tag.input.ts
@@ -1,10 +1,11 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsNotEmpty,   IsString } from 'class-validator';
+import { IsNotEmpty,   IsString, MaxLength } from 'class-validator';
 
 @InputType()
 export class CreateTagInput {
   @Field(() => String, { description: 'Tag (placeholder)' })
   @IsNotEmpty()
   @IsString()
+  @MaxLength(100)
   name: String;
 }
