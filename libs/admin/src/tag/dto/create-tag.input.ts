e800508aa63eed1cdb725f265e90e4a3c8287942
Yan David <ygomezcoba@gmail.com>
2021-08-25 11:51:22 -0400

    FB-14 refactor: tag model DTO validation

--- libs/admin/src/tag/dto/create-tag.input.ts
+++ libs/admin/src/tag/dto/create-tag.input.ts
@@ -1,7 +1,10 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty,   IsString } from 'class-validator';
 
 @InputType()
 export class CreateTagInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'Tag (placeholder)' })
+  @IsNotEmpty()
+  @IsString()
+  name: String;
 }
