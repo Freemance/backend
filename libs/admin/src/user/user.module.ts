043903cef1e7fd1d1238d8ca62c826b1624a4705
Yoandry leon <yoandry@getauru.com>
2021-08-26 12:32:03 -0400

    FB-2 refactor:update user module

--- libs/admin/src/user/user.module.ts
+++ libs/admin/src/user/user.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common'
-import { UserService } from './user.service'
-import { UserResolver } from './user.resolver'
+import { UserResolver, UserService } from '.';
 
 @Module({
   providers: [UserResolver, UserService],
