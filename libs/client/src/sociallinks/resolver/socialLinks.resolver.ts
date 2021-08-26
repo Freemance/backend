2dbb165a60d42431082b56381eeccf4eea458c28
javierrod9191 <javierrod9191@gmail.com>
2021-08-26 08:43:39 -0500

    FB-12 chore: add SocialLinks providers

--- libs/client/src/sociallinks/resolver/socialLinks.resolver.ts
+++ libs/client/src/sociallinks/resolver/socialLinks.resolver.ts
@@ -1,32 +1,32 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql'
-import { CreateSociallinkInput, SocialLink, SocialLinksService, UpdateSociallinkInput } from '..'
+import { CreateSocialLinkInput, SocialLink, SocialLinksService, UpdateSocialLinkInput } from '..'
 
 @Resolver(() => SocialLink)
 export class SocialLinksResolver {
-  constructor(private readonly sociallinksService: SocialLinksService) {}
+  constructor(private readonly service: SocialLinksService) {}
 
   @Mutation(() => SocialLink)
-  createSociallink(@Args('createSociallinkInput') createSociallinkInput: CreateSociallinkInput) {
-    return this.sociallinksService.create(createSociallinkInput)
+  createSocialLink(@Args('input') input: CreateSocialLinkInput) {
+    return this.service.createSocialLink(input)
   }
 
-  @Query(() => [SocialLink], { name: 'sociallinks' })
-  findAll() {
-    return this.sociallinksService.findAll()
+  @Query(() => [SocialLink], { name: 'socialLinks' })
+  getAllSocialLink() {
+    return this.service.getAllSocialLink()
   }
 
-  @Query(() => SocialLink, { name: 'SocialLink' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.sociallinksService.findOne(id)
-  }
-
-  @Mutation(() => SocialLink)
-  updateSociallink(@Args('updateSociallinkInput') updateSociallinkInput: UpdateSociallinkInput) {
-    return this.sociallinksService.update(updateSociallinkInput.id, updateSociallinkInput)
+  @Query(() => SocialLink, { name: 'socialLink' })
+  getSocialLinkById(@Args('id', { type: () => Int }) id: number) {
+    return this.service.getSocialLinkById(id)
   }
 
   @Mutation(() => SocialLink)
-  removeSociallink(@Args('id', { type: () => Int }) id: number) {
-    return this.sociallinksService.remove(id)
+  updateSocialLink(@Args('id', { type: () => Int }) id: number, @Args('input') input: UpdateSocialLinkInput) {
+    return this.service.updateSocialLink(id, input)
+  }
+
+  @Mutation(() => Boolean)
+  deleteSocialLink(@Args('id', { type: () => Int }) id: number) {
+    return this.service.deleteSocialLink(id)
   }
 }
