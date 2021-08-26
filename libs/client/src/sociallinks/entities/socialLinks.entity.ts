b3d5acef3336d29a5903970266a534a2b00c163e
javierrod9191 <javierrod9191@gmail.com>
2021-08-26 13:41:51 -0500

    FB-12 fix: add profile field

--- libs/client/src/sociallinks/entities/socialLinks.entity.ts
+++ libs/client/src/sociallinks/entities/socialLinks.entity.ts
@@ -1,5 +1,7 @@
-import { BaseModel } from '@feature/core'
 import { ObjectType, Field } from '@nestjs/graphql'
+
+import { BaseModel } from '@feature/core'
+import { Profile } from '@feature/client'
 
 @ObjectType()
 export class SocialLink extends BaseModel {
@@ -14,4 +16,7 @@ export class SocialLink extends BaseModel {
 
   @Field({ description: 'Telegram link', nullable: true })
   telegram?: string
+
+  @Field(() => Profile, { description: 'Profile associated  to the proyect', nullable: true })
+  profile?: Profile
 }
