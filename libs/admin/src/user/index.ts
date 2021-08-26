4b0bb137fe83a8fabbc8b3e4ace000b04d3a56ed
Yoandry leon <yoandry@getauru.com>
2021-08-26 10:04:31 -0400

    FB-2 chore:create user crud default structure

---
+++ libs/admin/src/user/index.ts
@@ -0,0 +1,7 @@
+export * from './dto/create-user.input'
+export * from './dto/update-user.input'
+
+export * from './resolver/user.resolver'
+export * from './service/user.service'
+
+export * from './user.module'
