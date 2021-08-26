946a5b533e56ec5f63e270030c23de695fd2d4eb
javierrod9191 <javierrod9191@gmail.com>
2021-08-26 14:27:43 -0500

    refactor: rename export

--- libs/client/src/sociallinks/index.ts
+++ libs/client/src/sociallinks/index.ts
@@ -2,6 +2,6 @@ export * from './dto/create-socialLinks.input'
 export * from './dto/update-socialLinks.input'
 
 export * from './entities/socialLinks.entity'
-export * from './resolver/sociallinks.resolver'
+export * from './resolver/socialLinks.resolver'
 
-export * from './service/sociallinks.service'
+export * from './service/socialLinks.service'
