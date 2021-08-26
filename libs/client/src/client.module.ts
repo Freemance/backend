ea5a3b8eeb96bd7c677d93881d6a640d1359d125
javierrod9191 <javierrod9191@gmail.com>
2021-08-26 08:55:33 -0500

    Fb-5 chore: add scaffold for language

--- libs/client/src/client.module.ts
+++ libs/client/src/client.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ClientService } from './client.service'
 import { PortfolioModule } from './portfolio/portfolio.module'
+import { LanguageModule } from './language/language.module';
 
 @Module({
   providers: [ClientService],
   exports: [ClientService],
-  imports: [PortfolioModule],
+  imports: [PortfolioModule, LanguageModule],
 })
 export class ClientModule {}
