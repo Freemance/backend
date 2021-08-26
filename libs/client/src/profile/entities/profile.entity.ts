61fab8f72c1a3461173cfd21a71d54fa4e5fdf64
javierrod9191 <javierrod9191@gmail.com>
2021-08-26 15:14:44 -0500

    FB-6 chore: add profile entity + modified schema

--- libs/client/src/profile/entities/profile.entity.ts
+++ libs/client/src/profile/entities/profile.entity.ts
@@ -1,7 +1,62 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql'
+import { ObjectType, Field } from '@nestjs/graphql'
+import { BaseModel } from '@feature/core'
+import { Tag } from '@feature/admin'
+import { User } from '@feature/auth'
+import { ProfileSkill, SocialLink, Job, Course, Portfolio, Language } from '@feature/client'
 
 @ObjectType()
-export class Profile {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number
+export class Profile extends BaseModel {
+  @Field({ description: 'Slyck User name' })
+  slykUser: string
+
+  @Field({ description: 'JobTitle name', nullable: true })
+  jobTitle?: string
+
+  @Field({ description: 'Bio description', nullable: true })
+  bio?: string
+
+  @Field({ description: 'Country name', nullable: true })
+  country?: string
+
+  @Field({ description: 'City name', nullable: true })
+  city?: string
+
+  @Field({ description: 'Address', nullable: true })
+  address?: string
+
+  @Field({ description: 'PostalCode', nullable: true })
+  postalCode?: string
+
+  @Field({ description: 'Place of birth', nullable: true })
+  placeOfBirth?: string
+
+  @Field(() => Date, { description: 'Date of birth', nullable: true })
+  dateOfBirth?: Date
+
+  @Field({ description: 'Phone number', nullable: true })
+  phone?: string
+
+  @Field(() => User, { description: 'User associated  to the profile' })
+  user: User
+
+  @Field(() => Tag, { description: 'Tag associated  to the profile' })
+  tag: Tag
+
+  @Field(() => [ProfileSkill], { description: 'Skills associated  to the profile' })
+  skills: [ProfileSkill]
+
+  @Field(() => SocialLink, { description: 'Social links associated  to the profile', nullable: true })
+  socialLinks?: SocialLink
+
+  @Field(() => [Job], { description: 'Jobs associated  to the profile', nullable: true })
+  employmentHistory?: [Job]
+
+  @Field(() => [Course], { description: 'Courses associated  to the profile', nullable: true })
+  courses?: [Course]
+
+  @Field(() => [Portfolio], { description: 'Portfolio associated  to the profile', nullable: true })
+  portfolioItem?: [Portfolio]
+
+  @Field(() => [Language], { description: 'Languages associated  to the profile', nullable: true })
+  languages?: [Language]
 }
