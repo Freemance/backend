5ff5c5573ff024c09fce9486e95468e302b52909
javierrod9191 <javierrod9191@gmail.com>
2021-08-26 15:31:02 -0500

    chore: run migration + import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { AuthModule } from '@feature/auth/auth.module'
-import { CoreModule, DataModule } from '@feature/core'
 import { Module } from '@nestjs/common'
+import { AdminModule } from '@feature/admin'
+import { AuthModule } from '@feature/auth/auth.module'
+import { ClientModule } from '@feature/client'
+import { CoreModule, DataModule } from '@feature/core'
 
 @Module({
-  imports: [CoreModule, DataModule, AuthModule],
+  imports: [CoreModule, DataModule, AuthModule, ClientModule, AdminModule],
   controllers: [],
   providers: [],
 })
