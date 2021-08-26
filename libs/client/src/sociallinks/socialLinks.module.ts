6dc93a07e0fc0d94bbe19cd0ee8585483538d043
javierrod9191 <javierrod9191@gmail.com>
2021-08-26 08:45:50 -0500

    FB-12 chore: import resolver+service to the module

--- libs/client/src/sociallinks/socialLinks.module.ts
+++ libs/client/src/sociallinks/socialLinks.module.ts
@@ -1,8 +1,9 @@
 import { DataModule } from '@feature/core'
 import { Module } from '@nestjs/common'
+import { SocialLinksResolver, SocialLinksService } from '.'
 
 @Module({
   imports: [DataModule],
-  providers: [],
+  providers: [SocialLinksService, SocialLinksResolver],
 })
 export class SocialLinksModule {}
