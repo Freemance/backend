fb08a184a7f619441f5b1995d1e3b3fb2fc9977b
javierrod9191 <javierrod9191@gmail.com>
2021-08-26 14:39:58 -0500

    FB-6 chore: add profile scaffold

--- libs/client/src/client.module.ts
+++ libs/client/src/client.module.ts
@@ -5,8 +5,9 @@ import { JobModule } from './job/job.module'
 import { LanguageModule } from './language/language.module'
 import { CourseModule } from './course/course.module'
 import { ProfileSkillModule } from './profile-skill/profile-skill.module'
+import { ProfileModule } from './profile/profile.module'
 
 @Module({
-  imports: [PortfolioModule, LanguageModule, ProfileSkillModule, JobModule, CourseModule],
+  imports: [PortfolioModule, LanguageModule, ProfileSkillModule, JobModule, CourseModule, ProfileModule],
 })
 export class ClientModule {}
---
+++ libs/client/src/profile/index.ts
@@ -0,0 +1,8 @@
+export * from './dto/create-profile.input'
+export * from './dto/update-profile.input'
+
+export * from './entities/profile.entity'
+export * from './resolver/profile.resolver'
+export * from './service/profile.service'
+
+export * from './profile.module'
