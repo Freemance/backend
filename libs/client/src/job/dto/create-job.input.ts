4962ce0a9954cfeb251fff7890cd216ec04313c7
Yan David <ygomezcoba@gmail.com>
2021-08-26 13:56:50 -0400

    FB-13 refactor: add the rest of the fields to DTO

--- libs/client/src/job/dto/create-job.input.ts
+++ libs/client/src/job/dto/create-job.input.ts
@@ -1,10 +1,32 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsDate, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 @InputType()
 export class CreateJobInput {
-  @Field(() => String, { description: 'Job (placeholder)' })
+  @Field(() => String, { description: 'Name of the job' })
   @IsNotEmpty()  
   @IsString()
+  @MaxLength(100)
   name: string
+
+  @Field({ description: 'Company associated  to the job' })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  company?: string
+
+  @Field({ description: 'Description associated  to the job' })
+  @IsString()
+  @MaxLength(500)
+  description?: string  
+
+  @Field(() => Date, { description: 'Start date associated  to the job' })
+  @IsDate()
+  @IsNotEmpty()
+  startDate?: Date
+
+  @Field(() => Date, { description: 'End date associated  to the job' })
+  @IsDate()
+  @IsNotEmpty()
+  endDate?: Date
 }
