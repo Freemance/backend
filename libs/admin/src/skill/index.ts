ad0e386726343306dc21c173096c31f42c9217aa
javierrod9191 <javierrod9191@gmail.com>
2021-08-26 09:33:23 -0500

    FB-7 fix: lvl.enum

--- libs/admin/src/skill/index.ts
+++ libs/admin/src/skill/index.ts
@@ -2,7 +2,7 @@ export * from './dto/create-skill.input'
 export * from './dto/update-skill.input'
 
 export * from './entities/skill.entity'
-export * from './entities/lvl'
+export * from './entities/lvl.enum'
 
 export * from './resolver/skill.resolver'
 export * from './service/skill.service'
