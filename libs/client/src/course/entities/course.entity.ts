6136ab1ee4613a4478ccecfb960af4f4aac6aae8
javierrod9191 <javierrod9191@gmail.com>
2021-08-26 13:43:29 -0500

    FB-10 chore: add course.entity

--- libs/client/src/course/entities/course.entity.ts
+++ libs/client/src/course/entities/course.entity.ts
@@ -1,7 +1,21 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql'
+import { BaseModel } from '@feature/core'
+import { Profile } from '@feature/client'
 
 @ObjectType()
-export class Course {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class Course extends BaseModel {
+  @Field({ description: 'Course name', nullable: true })
+  course?: string
+
+  @Field({ description: 'Institution name', nullable: true })
+  institution?: string
+
+  @Field(() => Date, { description: 'Start date associated  to the course', nullable: true })
+  startDate?: Date
+
+  @Field(() => Date, { description: 'End date associated  to the course', nullable: true })
+  endDate?: Date
+
+  @Field(() => Profile, { description: 'Profile associated  to the course', nullable: true })
+  profile?: Profile
 }
