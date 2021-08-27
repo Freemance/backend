31c275bb4ca87d69d2591f87fdfc543d45d388e8
Yan David <ygomezcoba@gmail.com>
2021-08-27 12:38:53 -0400

    FB-16 refactor: add GraphQLModule to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AdminModule } from '@feature/admin'
 import { AuthModule } from '@feature/auth/auth.module'
 import { ClientModule } from '@feature/client'
 import { CoreModule, DataModule } from '@feature/core'
+import { GraphQLModule } from '@nestjs/graphql'
 
 @Module({
-  imports: [CoreModule, DataModule, AuthModule, AdminModule, ClientModule],
+  imports: [
+    CoreModule,
+    DataModule,
+    AuthModule,
+    AdminModule,
+    ClientModule,
+    GraphQLModule.forRoot({
+      introspection: true,
+      playground: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
