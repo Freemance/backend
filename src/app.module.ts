ad0265d378f1cbb1c838969424e7bebfdb290358
Yan David <ygomezcoba@gmail.com>
2021-08-30 22:23:07 -0400

    FB-17 refactor: import LoggerMiddleware to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { AdminModule } from '@feature/admin'
 import { AuthModule } from '@feature/auth/auth.module'
 import { ClientModule } from '@feature/client'
 import { CoreModule, DataModule } from '@feature/core'
+import { LoggerMiddleware } from '../libs/middleware/logger/logger.middleware'
 
 @Module({
   imports: [CoreModule, DataModule, AuthModule, AdminModule, ClientModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*')
+  }
+}
