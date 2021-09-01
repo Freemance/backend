92baaa78da33fa661ec1a9b16d00e7ce73436379
javierrod9191 <javierrod9191@gmail.com>
2021-09-01 10:55:00 -0500

    FB-19 chore: add inProgressField + optional fields

--- libs/client/src/job/entities/job.entity.ts
+++ libs/client/src/job/entities/job.entity.ts
@@ -1,7 +1,7 @@
 import { ObjectType, Field } from '@nestjs/graphql'
 import { BaseModel } from '@feature/core'
 import { Profile } from '@feature/client'
-import { IsDate, IsString } from 'class-validator'
+import { IsBoolean, IsDate, IsString } from 'class-validator'
 
 @ObjectType()
 export class Job extends BaseModel {
@@ -17,14 +17,18 @@ export class Job extends BaseModel {
   @IsString()
   description?: string
 
-  @Field(() => Date, { description: 'Start date associated  to the job', nullable: true })
+  @Field(() => Date, { description: 'Start date associated  to the job' })
   @IsDate()
   startDate: Date
 
   @Field(() => Date, { description: 'End date associated  to the job', nullable: true })
   @IsDate()
-  endDate: Date
+  endDate?: Date
+
+  @Field(() => Boolean, { description: 'Checks if the jobs is still in progress, default is false', nullable: true })
+  @IsBoolean()
+  inProgress: boolean
 
   @Field(() => Profile, { description: 'Profiles asociated to Jobs', nullable: true })
-  profile: Profile
+  profile?: Profile
 }
