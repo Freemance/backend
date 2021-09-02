7bdb714ce3cb463401841e426945472c304cc94e
javierrod9191 <javierrod9191@gmail.com>
2021-09-02 17:02:26 -0500

    BF-21 chore: add multimedia serivces

--- libs/client/src/multimedia/service/multimedia.service.ts
+++ libs/client/src/multimedia/service/multimedia.service.ts
@@ -1,21 +1,52 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common'
+import { DataService } from '@feature/core'
 import { CreateMultimediaInput } from '../dto/create-multimedia.input'
 
 @Injectable()
 export class MultimediaService {
-  create(input: CreateMultimediaInput) {
-    return 'This action adds a new multimedia'
+  constructor(private readonly _service: DataService) {}
+
+  async getAllMultimedias() {
+    return this._service.course.findMany({ orderBy: { id: 'asc' } })
   }
 
-  findAll() {
-    return `This action returns all multimedia`
+  async createMultimedia(profileId: number, input: CreateMultimediaInput) {
+    return this._service.multimedia.create({
+      data: {
+        created_by: profileId,
+        ...input,
+      },
+    })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} multimedia`
+  async getMultimediaById(id: number) {
+    const found = await this._service.multimedia.findUnique({ where: { id } })
+    if (!found) {
+      throw new NotFoundException(`Multimedia with id: ${id} not found`)
+    }
+    return found
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} multimedia`
+  async removeMultimediaByUser(id: number, profileId: number) {
+    const found = await this.getMultimediaById(id)
+    if (found.created_by !== profileId) {
+      throw new UnauthorizedException()
+    }
+    const deleted = await this._service.multimedia.delete({
+      where: {
+        id: found.id,
+      },
+    })
+    return !!deleted
+  }
+
+  async removeMultimediaByAdmin(id: number) {
+    const found = await this.getMultimediaById(id)
+    const deleted = await this._service.multimedia.delete({
+      where: {
+        id: found.id,
+      },
+    })
+    return !!deleted
   }
 }
