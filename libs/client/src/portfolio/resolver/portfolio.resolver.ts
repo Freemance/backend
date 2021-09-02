bfe84425b50125330a033a69d367908feef8467b
Yan David <ygomezcoba@gmail.com>
2021-09-01 21:36:30 -0400

    FB-20 refactor: add decorators and profile params

--- libs/client/src/portfolio/resolver/portfolio.resolver.ts
+++ libs/client/src/portfolio/resolver/portfolio.resolver.ts
@@ -3,33 +3,42 @@ import { Portfolio } from './../entities/portfolio.entity'
 import { PortfolioService } from '../service/portfolio.service'
 import { CreatePortfolioInput } from '../dto/create-portfolio.input'
 import { UpdatePortfolioInput } from '../dto/update-portfolio.input'
+import { UseGuards } from '@nestjs/common'
+import { GqlAuthGuard, Role, Roles, RolesGuard, UserEntity, User } from '@feature/auth'
 
+@UseGuards(RolesGuard)
+@Roles(Role.USER)
+@UseGuards(GqlAuthGuard)
 @Resolver(() => Portfolio)
 export class PortfolioResolver {
   constructor(private readonly _service: PortfolioService) {}
 
-  @Mutation(() => Portfolio, { nullable: true })
-  createPortfolio(@Args('input') input: CreatePortfolioInput) {
-    return this._service.createPortfolio(input)
+  @Query(() => [Portfolio], { name: 'ProfilePortfolios', nullable: 'items' })
+  getProfilePortfolioItems(@UserEntity() user: User) {
+    return this._service.getProfilePortfolioItems(user.profile.id)
   }
 
-  @Query(() => [Portfolio], { name: 'portfolios', nullable: 'items' })
-  getAllPortfolio() {
-    return this._service.getAllPortfolio()
+  @Mutation(() => Portfolio, { name: 'ProfileCreatePortfolio', nullable: true })
+  createProfilePortfolio(@UserEntity() user: User, @Args('input') input: CreatePortfolioInput) {
+    return this._service.createProfilePortfolio(user.profile.id, input)
   }
 
-  @Query(() => Portfolio, { name: 'portfolio', nullable: true })
-  getPortfolioById(@Args('id', { type: () => Int }) id: number) {
-    return this._service.getPortfolioById(id)
+  @Query(() => Portfolio, { name: 'ProfilePortfolioById', nullable: true })
+  getProfilePortfolioById(@UserEntity() user: User, @Args('id', { type: () => Int }) id: number) {
+    return this._service.getProfilePortfolioById(id, user.profile.id)
   }
 
-  @Mutation(() => Portfolio, { nullable: true })
-  updatePortfolio(@Args('id', { type: () => Int }) id: number, @Args('input') input: UpdatePortfolioInput) {
-    return this._service.updatePortfolio(id, input)
+  @Mutation(() => Portfolio, { name: 'ProfileUpdatePortfolio', nullable: true })
+  updateProfilePortfolio(
+    @UserEntity() user: User,
+    @Args('id', { type: () => Int }) id: number,
+    @Args('input') input: UpdatePortfolioInput,
+  ) {
+    return this._service.updateProfilePortfolio(id, user.profile.id, input)
   }
 
-  @Mutation(() => Boolean, { nullable: true })
-  deletePortfolio(@Args('id', { type: () => Int }) id: number) {
-    return this._service.deletePortfolio(id)
+  @Mutation(() => Boolean, { name: 'ProfileDeletePortfolio', nullable: true })
+  deleteProfilePortfolio(@UserEntity() user: User, @Args('id', { type: () => Int }) id: number) {
+    return this._service.deleteProfilePortfolio(id, user.profile.id)
   }
 }
