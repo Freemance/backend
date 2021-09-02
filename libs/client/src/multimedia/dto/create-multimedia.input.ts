9da378160032fc0eed599472b453f0819c11f180
javierrod9191 <javierrod9191@gmail.com>
2021-09-02 16:18:55 -0500

    FB-21 chore: add multimedia.entity + create dto

--- libs/client/src/multimedia/dto/create-multimedia.input.ts
+++ libs/client/src/multimedia/dto/create-multimedia.input.ts
@@ -1,7 +1,38 @@
-import { InputType, Int, Field } from '@nestjs/graphql'
+import { InputType, Field } from '@nestjs/graphql'
+import { IsBoolean, IsString, MaxLength } from 'class-validator'
 
 @InputType()
 export class CreateMultimediaInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number
+  @Field({ description: 'Name of the file', nullable: true })
+  @IsString()
+  @MaxLength(350)
+  file_name?: string
+
+  @Field({ description: 'Path of the file', nullable: true })
+  @IsString()
+  file_path?: string
+
+  @Field({ description: 'Size of the file', nullable: true })
+  @IsString()
+  @MaxLength(50)
+  file_size?: string
+
+  @Field({ description: 'Type of the file', nullable: true })
+  @IsString()
+  @MaxLength(50)
+  file_type?: string
+
+  @Field({ description: 'Extension of the file', nullable: true })
+  @IsString()
+  @MaxLength(50)
+  file_extension?: string
+
+  @Field({ description: 'Height of the file', nullable: true })
+  @IsString()
+  @MaxLength(50)
+  file_height?: string
+
+  @Field({ description: 'Status of the file ' })
+  @IsBoolean()
+  status: boolean
 }
--- libs/client/src/multimedia/entities/multimedia.entity.ts
+++ libs/client/src/multimedia/entities/multimedia.entity.ts
@@ -1,15 +1,29 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql'
+import { BaseModel } from '@feature/core'
 
 @ObjectType()
-export class Multimedia {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
+export class Multimedia extends BaseModel {
+  @Field(() => Int, { description: 'Id of the user)' })
   created_by: number
+
+  @Field({ description: 'Name of the file', nullable: true })
+  file_name?: string
+
+  @Field({ description: 'Path of the file', nullable: true })
+  file_path?: string
+
+  @Field({ description: 'Size of the file', nullable: true })
+  file_size?: string
+
+  @Field({ description: 'Type of the file', nullable: true })
+  file_type?: string
+
+  @Field({ description: 'Extension of the file', nullable: true })
+  file_extension?: string
+
+  @Field({ description: 'Height of the file', nullable: true })
+  file_height?: string
+
+  @Field({ description: 'Status of the file ' })
+  status: boolean
 }
-// created_by
-// file_name
-// file_path
-// file_size
-// file_type
-// file_extension
-// file_height
-// status
