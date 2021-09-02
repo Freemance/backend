25cc67a92ba0662150bbcf8d7c0f882dfe8878dc
Yoandry leon <yoandry@getauru.com>
2021-09-01 21:20:23 -0400

    fix:format

--- libs/client/src/course/resolver/course.resolver.spec.ts
+++ libs/client/src/course/resolver/course.resolver.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { CourseResolver } from './course.resolver'
-import { CourseService } from './course.service'
+import { CourseService } from '../service/course.service'
 
 describe('CourseResolver', () => {
   let resolver: CourseResolver
