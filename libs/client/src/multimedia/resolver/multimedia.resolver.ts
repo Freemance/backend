192a5f505ecd5f49be37a9c6dd57765950749385
Yoandry leon <yoandry@getauru.com>
2021-09-03 22:13:56 -0400

    refactor:update upload files resover

--- libs/client/src/multimedia/resolver/multimedia.resolver.ts
+++ libs/client/src/multimedia/resolver/multimedia.resolver.ts
@@ -2,17 +2,30 @@ import { UseGuards } from '@nestjs/common'
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql'
 
 import { FileUpload, GraphQLUpload } from 'graphql-upload'
+import { createWriteStream } from 'fs'
 import { GqlAuthGuard, Role, Roles, RolesGuard, User, UserEntity } from '@feature/auth'
 import { MultimediaService } from './../service/multimedia.service'
 import { Multimedia } from '../entities/multimedia.entity'
 import { CreateMultimediaInput } from '../dto/create-multimedia.input'
-import { createWriteStream } from 'fs'
-@UseGuards(GqlAuthGuard)
+
 @Resolver(() => Multimedia)
 export class MultimediaResolver {
   constructor(private readonly _service: MultimediaService) {}
 
-  @UseGuards(RolesGuard)
+  @Mutation(() => Boolean)
+  async uploadFile(
+    @Args({ name: 'file', type: () => GraphQLUpload })
+    { createReadStream, filename }: FileUpload,
+  ): Promise<boolean> {
+    return new Promise(async (resolve, reject) =>
+      createReadStream()
+        .pipe(createWriteStream(`./uploads/${filename}`))
+        .on('finish', () => resolve(true))
+        .on('error', () => reject(false)),
+    )
+  }
+
+  /*@UseGuards(RolesGuard)
   @Roles(Role.USER)
   @Mutation(() => Multimedia || Boolean, { name: 'CreateMultimedia' })
   createMultimedia(
@@ -56,5 +69,5 @@ export class MultimediaResolver {
   @Mutation(() => Boolean, { name: 'DeleteMultimediaByAdmin' })
   deleteMultimediaByAdmin(@Args('id', { type: () => Int }) id: number) {
     return this._service.deleteMultimediaByAdmin(id)
-  }
+  }*/
 }
