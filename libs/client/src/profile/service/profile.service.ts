dceea2cf616658ac2965b82d0eb6d70281ab8f65
javierrod9191 <javierrod9191@gmail.com>
2021-09-05 12:39:43 -0500

    FB-22 chore: add services for update profile

--- libs/client/src/profile/service/profile.service.ts
+++ libs/client/src/profile/service/profile.service.ts
@@ -1,12 +1,19 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { DataService, findManyCursorConnection } from '@feature/core'
-import { CreateProfileInput } from '../dto/create-profile.input'
-import { UpdateProfileInput } from '../dto/update-profile.input'
+import { UpdateBasicProfileInput } from '../dto/update-basicProfile.input'
 
 @Injectable()
 export class ProfileService {
   constructor(private readonly _service: DataService) {}
-  private includes = { tag: true, skills: true }
+  private includes = {
+    tag: true,
+    skills: true,
+    socialLinks: true,
+    employmentHistory: true,
+    courses: true,
+    portfolioItem: true,
+    languages: true,
+  }
 
   async filter(after: number, before: number, first: number, last: number, query: string, orderBy) {
     const a = await findManyCursorConnection(
@@ -139,7 +146,51 @@ export class ProfileService {
     return a
   }
 
-  async getProfiles() {
-    return await this._service.profile.findMany()
+  async getProfileById(profileId: number) {
+    const found = await this._service.profile.findUnique({
+      include: this.includes,
+      where: { id: profileId },
+    })
+    if (!found) {
+      throw new NotFoundException(`Profile with id: ${profileId} not found`)
+    }
+    return found
+  }
+
+  async updateBasicProfileInfo(profileId: number, input: UpdateBasicProfileInput, avatar?: string) {
+    const found = await this.getProfileById(profileId)
+    return this._service.profile.update({
+      include: this.includes,
+      where: { id: found.id },
+      data: {
+        ...input,
+        avatar: avatar ? avatar : found.avatar,
+      },
+    })
+  }
+
+  async updateProfileSkills(profileId: number, skillsId: [{ id: number }]) {
+    const found = await this.getProfileById(profileId)
+    return this._service.profile.update({
+      include: this.includes,
+      where: { id: found.id },
+      data: {
+        skills: {
+          connect: skillsId,
+        },
+      },
+    })
+  }
+  async updateProfileTag(profileId: number, tagId: number) {
+    const found = await this.getProfileById(profileId)
+    return this._service.profile.update({
+      include: this.includes,
+      where: { id: found.id },
+      data: {
+        tag: {
+          connect: { id: tagId },
+        },
+      },
+    })
   }
 }
