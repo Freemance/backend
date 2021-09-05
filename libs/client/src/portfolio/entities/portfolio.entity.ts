44024797caf8903282ecbb43fa07acc0d7f32e9e
javierrod9191 <javierrod9191@gmail.com>
2021-09-05 15:00:35 -0500

    FB-22 refactor: add more fields

--- libs/client/src/portfolio/entities/portfolio.entity.ts
+++ libs/client/src/portfolio/entities/portfolio.entity.ts
@@ -1,11 +1,11 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql'
+import { ObjectType, Field } from '@nestjs/graphql'
 
 import { BaseModel } from '@feature/core'
 import { Skill } from '@feature/admin'
 
 @ObjectType()
 export class Portfolio extends BaseModel {
-  @Field({ description: 'Name of the proyect' })
+  @Field({ description: 'Name of the proyect', nullable: true })
   proyect?: string
 
   @Field({ description: 'Link associated to the proyect', nullable: true })
@@ -14,12 +14,15 @@ export class Portfolio extends BaseModel {
   @Field({ description: 'Description associated to the proyect', nullable: true })
   description?: string
 
-  @Field(() => [Skill], { description: 'Description associated to the proyect', nullable: true })
-  skills?: [Skill]
+  @Field({ description: 'Screenshots associated to the proyect', nullable: true })
+  screenshts: [string]
 
   @Field(() => Date, { description: 'Start date associated to the proyect' })
   startDate?: Date
 
   @Field(() => Date, { description: 'End date associated to the proyect' })
   endDate?: Date
+
+  @Field(() => [Skill], { description: 'Skills associated to the proyect', nullable: true })
+  skills?: [Skill]
 }
