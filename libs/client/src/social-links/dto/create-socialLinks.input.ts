cf7e3dc64cf559ca5a6903a244891e87ca212352
javierrod9191 <javierrod9191@gmail.com>
2021-09-04 22:55:21 -0500

    Fb-22 chore: add more fields to SocialLink

--- libs/client/src/social-links/dto/create-socialLinks.input.ts
+++ libs/client/src/social-links/dto/create-socialLinks.input.ts
@@ -5,25 +5,73 @@ import { IsOptional, IsString, MaxLength } from 'class-validator'
 export class CreateSocialLinkInput {
   @Field({ description: 'Linkendin link', nullable: true })
   @IsString()
-  @MaxLength(150)
+  @MaxLength(350)
   @IsOptional()
   linkedin?: string
 
   @Field({ description: 'Instagram link', nullable: true })
   @IsString()
-  @MaxLength(150)
+  @MaxLength(350)
   @IsOptional()
   instagram?: string
 
   @Field({ description: 'Facebook link', nullable: true })
   @IsString()
-  @MaxLength(150)
+  @MaxLength(350)
   @IsOptional()
   facebook?: string
 
   @Field({ description: 'Telegram link', nullable: true })
   @IsString()
-  @MaxLength(150)
+  @MaxLength(350)
   @IsOptional()
   telegram?: string
+
+  @Field({ description: 'Twitter link', nullable: true })
+  @IsString()
+  @MaxLength(350)
+  @IsOptional()
+  twitter?: string
+
+  @Field({ description: 'Whatsapp link', nullable: true })
+  @IsString()
+  @MaxLength(350)
+  @IsOptional()
+  whatsapp?: string
+
+  @Field({ description: 'GooglePlus link', nullable: true })
+  @IsString()
+  @MaxLength(350)
+  @IsOptional()
+  googlePlus?: string
+
+  @Field({ description: 'Slack link', nullable: true })
+  @IsString()
+  @MaxLength(350)
+  @IsOptional()
+  slack?: string
+
+  @Field({ description: 'github link', nullable: true })
+  @IsString()
+  @MaxLength(350)
+  @IsOptional()
+  github?: string
+
+  @Field({ description: 'Youtube link', nullable: true })
+  @IsString()
+  @MaxLength(350)
+  @IsOptional()
+  youtube?: string
+
+  @Field({ description: 'Behance link', nullable: true })
+  @IsString()
+  @MaxLength(350)
+  @IsOptional()
+  behance?: string
+
+  @Field({ description: 'Dribble link', nullable: true })
+  @IsString()
+  @MaxLength(350)
+  @IsOptional()
+  dribbble?: string
 }
