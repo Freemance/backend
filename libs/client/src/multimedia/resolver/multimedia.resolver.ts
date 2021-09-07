2cd6aca1048bfb5146cea6ec8d4d6482a05c0db1
javierrod9191 <javierrod9191@gmail.com>
2021-09-07 14:37:09 -0500

    chore: modify upload file

--- libs/client/src/multimedia/resolver/multimedia.resolver.ts
+++ libs/client/src/multimedia/resolver/multimedia.resolver.ts
@@ -1,55 +1,24 @@
 import { UseGuards } from '@nestjs/common'
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql'
 
-import { FileUpload, GraphQLUpload } from 'graphql-upload'
-import { createWriteStream } from 'fs'
 import { GqlAuthGuard, Role, Roles, RolesGuard, User, UserEntity } from '@feature/auth'
+import { FileUpload, GraphQLUpload } from 'graphql-upload'
 import { MultimediaService } from './../service/multimedia.service'
 import { Multimedia } from '../entities/multimedia.entity'
-import { CreateMultimediaInput } from '../dto/create-multimedia.input'
 
 @Resolver(() => Multimedia)
 export class MultimediaResolver {
   constructor(private readonly _service: MultimediaService) {}
 
-  @Mutation(() => Boolean)
-  async uploadFile(
-    @Args({ name: 'file', type: () => GraphQLUpload })
-    { createReadStream, filename }: FileUpload,
-  ): Promise<boolean> {
-    return new Promise(async (resolve, reject) =>
-      createReadStream()
-        .pipe(createWriteStream(`./uploads/${filename}`))
-        .on('finish', () => resolve(true))
-        .on('error', () => reject(false)),
-    )
-  }
-
-  /*@UseGuards(RolesGuard)
+  @UseGuards(GqlAuthGuard, RolesGuard)
   @Roles(Role.USER)
-  @Mutation(() => Multimedia || Boolean, { name: 'CreateMultimedia' })
-  createMultimedia(
+  @Mutation(() => Multimedia)
+  async uploadFile(
     @UserEntity() user: User,
-    @Args({ name: 'file', type: () => GraphQLUpload }) { createReadStream, filename }: FileUpload,
-  ): Promise<Multimedia | boolean> {
-    return new Promise(async (resolve, reject) =>
-      createReadStream()
-        .pipe(createWriteStream(`./uploads/${filename}`))
-        .on('finish', () => {
-          const input: CreateMultimediaInput = {
-            file_path: `./uploads/${filename}`,
-            file_name: filename,
-            // file_extension: metadata.file_extension,
-            // file_height: metadata.file_height,
-            // file_size: metadata.file_size,
-            // file_type: metadata.file_type,
-            status: true,
-          }
-          console.log(input)
-          return resolve(this._service.createMultimedia(user.profile.id, input))
-        })
-        .on('error', () => reject(false)),
-    )
+    @Args({ name: 'file', type: () => GraphQLUpload })
+    file: FileUpload,
+  ) {
+    return this._service.saveMultimedia(user, file)
   }
 
   @Query(() => [Multimedia], { name: 'GetAllMultimedias' })
@@ -69,5 +38,5 @@ export class MultimediaResolver {
   @Mutation(() => Boolean, { name: 'DeleteMultimediaByAdmin' })
   deleteMultimediaByAdmin(@Args('id', { type: () => Int }) id: number) {
     return this._service.deleteMultimediaByAdmin(id)
-  }*/
+  }
 }
