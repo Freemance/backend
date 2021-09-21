efabc79cff0c558e2d8189df697f0edebf07ac96
Yan David <ygomezcoba@gmail.com>
2021-09-21 09:36:20 -0400

    FB-23 chore: add array for job testing service

--- libs/client/src/job/service/job.service.spec.ts
+++ libs/client/src/job/service/job.service.spec.ts
@@ -1,12 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { JobService } from './job.service'
 
+import { DataService } from '@feature/core'
+
+const jobArray = [
+  {
+    id: 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    name: 'Journalist',
+    company: 'Company One',
+    description: 'Best job ever',
+    startDate: new Date(),
+    endDate: new Date(),
+    inProgress: true,
+    profileId: 1,
+  },
+  {
+    id: 2,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    name: 'Journalist 1',
+    company: 'Company Two',
+    description: 'Best job ever 1',
+    startDate: new Date(),
+    endDate: new Date(),
+    inProgress: true,
+    profileId: 1,
+  },
+  {
+    id: 3,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    name: 'Journalist 2',
+    company: 'Company Three',
+    description: 'Best job ever 2',
+    startDate: new Date(),
+    endDate: new Date(),
+    inProgress: true,
+    profileId: 1,
+  },
+]
+
 describe('JobService', () => {
   let service: JobService
+  let prisma: DataService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [JobService],
+      providers: [
+        JobService,
+        {
+          provide: DataService,
+          useValue: db,
+        },
+      ],
     }).compile()
 
     service = module.get<JobService>(JobService)
