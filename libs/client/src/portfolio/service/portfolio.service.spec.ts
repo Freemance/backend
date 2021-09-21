2cfd789ae5d8a50b02085fdf0facf0d8c6dbe77f
Yan David <ygomezcoba@gmail.com>
2021-09-21 11:48:23 -0400

    FB-23 chore: add crud functions portfolio test

--- libs/client/src/portfolio/service/portfolio.service.spec.ts
+++ libs/client/src/portfolio/service/portfolio.service.spec.ts
@@ -1,5 +1,5 @@
-import { Skill } from '.prisma/client'
 import { DataService } from '@feature/core'
+import { NotFoundException, UnauthorizedException } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
 import { PortfolioService } from './portfolio.service'
 
@@ -78,21 +78,76 @@ describe('PortfolioService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PortfolioService],
+      providers: [
+        PortfolioService,
+        {
+          provide: DataService,
+          useValue: db,
+        },
+      ],
     }).compile()
 
     service = module.get<PortfolioService>(PortfolioService)
     prisma = module.get<DataService>(DataService)
   })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined()
-  })
-
   describe('getProfilePortfolioItems', () => {
-    it('should return a jobs array ', async () => {
+    it('should return a portfolios array ', async () => {
       const portfolios = await service.getProfilePortfolioItems(profileId)
       expect(portfolios).toEqual(portfolioArray)
+    })
+  })
+
+  describe('getProfilePortfolioById', () => {
+    it('should get a portfolio', () => {
+      expect(service.getProfilePortfolioById(portfolioId, profileId)).resolves.toEqual(secondPortfolio)
+    })
+
+    it('should return NotFoundException', () => {
+      jest.spyOn(prisma.portfolio, 'findUnique').mockResolvedValue(undefined)
+      expect(service.getProfilePortfolioById(8, profileId)).rejects.toThrow(NotFoundException)
+    })
+
+    it('should return UnauthorizedException', () => {
+      jest.spyOn(prisma.portfolio, 'findUnique').mockResolvedValue({ ...secondPortfolio, profileId: 2 })
+      expect(service.getProfilePortfolioById(portfolioId, profileId)).rejects.toThrow(UnauthorizedException)
+    })
+  })
+
+  describe('CreateProfilePortfolio', () => {
+    it('should successfully insert a portfolio', () => {
+      expect(service.createProfilePortfolio(profileId, { ...secondPortfolio })).resolves.toEqual(secondPortfolio)
+    })
+  })
+
+  describe('UpdateProfilePortfolio', () => {
+    it('should call the update method', async () => {
+      jest
+        .spyOn(prisma.portfolio, 'findUnique')
+        .mockResolvedValue({ ...secondPortfolio, createdAt: null, updatedAt: null })
+      const portfolio = await service.updateProfilePortfolio(portfolioId, profileId, { ...secondPortfolio })
+      expect(portfolio).toEqual(secondPortfolio)
+    })
+
+    it('should return NotFoundException', async () => {
+      jest.spyOn(prisma.portfolio, 'findUnique').mockResolvedValue(undefined)
+      await expect(service.updateProfilePortfolio(portfolioId, profileId, { ...secondPortfolio })).rejects.toThrow(
+        NotFoundException,
+      )
+    })
+  })
+
+  describe('DeleteProfilePortfolio', () => {
+    it('should return true', () => {
+      jest
+        .spyOn(prisma.portfolio, 'findUnique')
+        .mockResolvedValue({ ...secondPortfolio, createdAt: null, updatedAt: null })
+      expect(service.deleteProfilePortfolio(portfolioId, profileId)).resolves.toEqual(true)
+    })
+
+    it('should return Error', () => {
+      jest.spyOn(prisma.portfolio, 'delete').mockRejectedValueOnce(new Error('Wrong Delete Method.'))
+      expect(service.deleteProfilePortfolio(portfolioId, profileId)).rejects.toEqual(Error('Wrong Delete Method.'))
     })
   })
 })
