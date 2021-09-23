feb406384dafe2b42159aa50fd73a1bab583e3ad
Yan David <ygomezcoba@gmail.com>
2021-09-23 09:31:27 -0400

    Fb-23 chore: create profileArray in service test

--- libs/client/src/profile/service/profile.service.spec.ts
+++ libs/client/src/profile/service/profile.service.spec.ts
@@ -1,5 +1,66 @@
+import { ProfileStatus } from '.prisma/client'
 import { Test, TestingModule } from '@nestjs/testing'
 import { ProfileService } from './profile.service'
+
+const profileArray = [
+  {
+    id: 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    slykUser: 'Slyker One',
+    firstName: 'My first name',
+    lastName: 'My last name',
+    profileStatus: ProfileStatus.APPROVED,
+    avatar: 'My avatar',
+    jobTitle: 'My job title',
+    bio: 'My bio',
+    country: 'Not Cuba please',
+    city: 'My city',
+    address: 'My address',
+    postalCode: 'My postal code',
+    placeOfBirth: 'Someother that Cuba',
+    dateOfBirth: new Date(),
+    phone: '5665134982',
+  },
+  {
+    id: 2,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    slykUser: 'Slyker Two',
+    firstName: 'My first name',
+    lastName: 'My last name',
+    profileStatus: ProfileStatus.APPROVED,
+    avatar: 'My avatar',
+    jobTitle: 'My job title',
+    bio: 'My bio',
+    country: 'Not Cuba please',
+    city: 'My city',
+    address: 'My address',
+    postalCode: 'My postal code',
+    placeOfBirth: 'Someother that Cuba',
+    dateOfBirth: new Date(),
+    phone: '462384168',
+  },
+  {
+    id: 3,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    slykUser: 'Slyker Three',
+    firstName: 'My first name',
+    lastName: 'My last name',
+    profileStatus: ProfileStatus.APPROVED,
+    avatar: 'My avatar',
+    jobTitle: 'My job title',
+    bio: 'My bio',
+    country: 'Not Cuba please',
+    city: 'My city',
+    address: 'My address',
+    postalCode: 'My postal code',
+    placeOfBirth: 'Someother that Cuba',
+    dateOfBirth: new Date(),
+    phone: '593216523',
+  },
+]
 
 describe('ProfileService', () => {
   let service: ProfileService
